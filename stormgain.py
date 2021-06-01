451fec95f23e8d197d324fd7c5cadbed4b861596
Cappuchino <crkza1134@gmail.com>
2021-06-01 20:05:35 +0700

    CAPTCHA Evasion using fake User Agent

--- stormgain.py
+++ stormgain.py
@@ -22,7 +22,9 @@ fromA = config['stormgainsleepMIN']
 toB = config['stormgainsleepMAX']
 path = os.getcwd()+'\\'
 chrome_options = Options()
-chrome_options.add_argument('--window-size=1600x800')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument(path)
 
